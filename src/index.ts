5d91def93aa0ba12f758bf1437dd5c1d6f86dce2
Sam <enquiry.conaticus@gmail.com>
2024-03-17 10:13:49 +0000

    remove page file & add more sites

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,11 @@ import Crawler from "./crawler";
 
 const SAMPLE_SITES = [
     "https://asus.com",
-    // "https://ebay.com",
-    // "https://www.bbc.co.uk/news",
-    // "https://www.rust-lang.org/",
-    // "https://en.wikipedia.org/wiki/Car",
+    "https://ebay.com",
+    "https://www.bbc.co.uk/news",
+    "https://www.rust-lang.org/",
+    "https://en.wikipedia.org/wiki/Car",
 ];
-
-async function scrapeSite(url: string, page: Page) {}
 
 async function scrapeWeb() {
     const crawler = await Crawler.create(SAMPLE_SITES.length);
